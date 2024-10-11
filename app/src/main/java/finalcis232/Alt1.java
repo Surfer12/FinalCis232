1d252ffbbb88c1e2bd31c6f641aad27baf890edb
Surfer12 <ryanoatsie@outlook.com>
2024-10-11 07:52:44 -0700

    build gradle

--- app/src/main/java/finalcis232/Alt1.java
+++ app/src/main/java/finalcis232/Alt1.java
@@ -3,9 +3,9 @@ public class Alt1 {
     private boolean isPalindrome;
 
     public static boolean PalindromeFunctionMethod(String str) {
+        String inputWord = str;
         boolean isPalindrome = true; // Initialize as true
-        char[] inputWordCharArray = str.toLowerCase().toCharArray();
-        boolean isPalindrome = true;
+        char[] inputWordCharArray = inputWord.toLowerCase().toCharArray();
         for (int i = 0; i < inputWordCharArray.length / 2; i++) { // Check only half the array
             if (inputWordCharArray[i] != inputWordCharArray[inputWordCharArray.length - 1 - i]) {
                 isPalindrome = false; // Set to false if mismatch found
@@ -13,5 +13,10 @@ public class Alt1 {
             }
         }
         return isPalindrome;
+    }
+
+    public static void main(String[] args) {
+        String inputWord = "racecar";
+        System.out.println(PalindromeFunctionMethod(inputWord));
     }
 }
